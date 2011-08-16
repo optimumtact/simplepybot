3eae42461cdc82ed5f791ad9d75427b8ac68bdc4
Francis Devine <optimumtact@gmail.com>
2011-08-16 18:41:37 +1200

    removed old files

--- botframework.py
+++ botframework.py
@@ -23,14 +23,44 @@ def connect(server, port, nick, ident, host, realname):
 
 def isPrivMessage(msg):
   if "PRIVMSG" in msg:
-    return 1 
+     return 1 
   else:
     return 0
 
+def isServerMessage(msg, server):
+  if server in msg:
+   return 1
+  else:
+    return 0
 
-msglist={"francis PRIVMSG #bots :this is","this is not"}
+def isPing(msg):
+  msg=msg.split(" ")
+  if msg[1]=="PING":
+    return msg[2]
+  else:
+    return 0
+
+def cleanServerMessage(msg):
+  clean=msg.lstrip()
+  clean=clean.split(" ", 4)
+  print clean
+  clean[3]=clean[3].lstrip(":")
+  return clean
+
+server="segfault.net.nz"
+msglist={"francis PRIVMSG #bots :this is","this is not", ":segfault.net.nz 003 rachel :Your host is gay",
+         "PING :segfault.net.nz"}
 for msg in msglist:
   if (isPrivMessage(msg)):
     print cleanPrivMessage(msg)
   else:
     print msg+" is not a PRIVMSG"
+  if (isServerMessage(msg, server)):
+    print cleanServerMessage(msg)
+  else:
+    print msg+" is not a server message"
+  reply=isPing(msg)
+  if (reply!=0):
+    print reply
+  else:
+    print msg+" is not a ping"
