b2fc3b73a88a5148a24f5ae58faa24658f095411
Francis Devine <optimumtact@gmail.com>
2011-08-17 00:29:43 +1200

    added a whole bunch of extra functionality

--- bot.py
+++ bot.py
@@ -1,4 +1,4 @@
-
+#!/usr/bin/env python2
 import ircbot
 import re
 # Connection information
@@ -8,14 +8,15 @@ channel = '#bots'
 nick = 'quotebot'
 name = 'BotHerd'
 
-# Create a dictionary to store statistics in
-statistics = {}
-lineBuff={}
-
-
 # Create our bot class
 class QuoteBot ( ircbot.SingleServerIRCBot ):
-
+  def __init__(self, network, nick, name):
+    ircbot.SingleServerIRCBot.__init__(self, network, nick, name)
+    self.spartamode=False
+    self.linebuf={"Francis had a little lamb"}
+    self.quotereg=re.compile('^!quotes? \w+')
+    self.quitreg=re.complile(nick+':? quit')
+    self.spartareg=re.compile('this')
 # Join the channel when welcomed
   def on_welcome ( self, connection, event ):
     connection.join ( channel )
@@ -23,24 +24,35 @@ class QuoteBot ( ircbot.SingleServerIRCBot ):
 # React to channel messages
   def on_pubmsg ( self, connection, event ):
     source = event.source().split ( '!' ) [ 0 ]
-
 # compile the regex matchin the quotes command
-    reg=re.compile('^!quotes? \w')
-
 # Check for a !quotes command
-    if reg.match(event.arguments()[0])!=None:
-     print event.arguments()
+    if self.quotereg.match(event.arguments()[0])!=None:
+     result=event.arguments()[0].split(' ')
+     for line in self.linebuf:
+       if re.match(result[1], line)!=None:
+         connection.privmsg(channel, line)
      
 #check for a quit command 
-    elif event.arguments()[ 0 ]==nick+' quit':
+    elif self.quitreg.match(event.arguments()[0]):
       self.die('Remote User Kill');
-    elif event.arguments()[ 0 ]=='this':
-      connection.privmsg(channel, "IS")
-      connection.privmsg(channel, "SPARTA")
-      connection.privmsg(channel, "/me kicks name into pit")
-    else:
-      print event.arguments()
+#check for a this (Super secret function here)
+    elif self.spartareg.match(event.arguments()[0] and self.spartamode:
+      connection.privmsg(channel, 'IS')
+      connection.privmsg(channel, 'SPARTA')
+      connection.privmsg(channel, '\001ACTION kicks '+source+' into a pit\001')
+
+#debug printing work
+#    print event.source()
+#    print event.eventtype()
+#    print event.target()
+#    print event.arguments()
 # Create the bot
-bot = QuoteBot ( [( network, port )], nick, name )
+
+  def on_privmsg(self, connection, event):
+    if(self.spartamode==True):
+      self.spartamode=False
+    else:
+      self.spartamode=True
+bot = QuoteBot ([(network, port)], nick, name )
 bot.start()
 
