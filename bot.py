3f6625b647b602ee5256130cee61bd8116637d49
Francis Devine <optimumtact@gmail.com>
2011-12-05 17:13:25 +1300

    finally got quote functionality in place

--- bot.py
+++ bot.py
@@ -20,38 +20,51 @@ class QuoteBot ( ircbot.SingleServerIRCBot ):
     self.counter=0
     self.bufSize=100
     ircbot.SingleServerIRCBot.__init__(self, network, nick, name)
+
 # Join the channel when welcomed
   def on_welcome ( self, connection, event ):
     connection.join ( channel )
+  
+#turn event objects into nicely formatted strings
+  def formatForDisplay(self, event):
+    displaystring=event.source().split('!')[0]+':'+event.arguments()[0]
+    return displaystring
 
 #handle reporting of matching quotes
-  def reportMatches(self, pattern):
-    for line in self.linebuf:
-      print line
+  def reportMatches(self, pattern, connection):
+      matchbuf=[]
+      for event in self.linebuf:
+        if re.search(pattern, event.arguments()[0])!=None:
+          matchbuf.append(self.formatForDisplay(event))
+      if (len(matchbuf)>6):
+        connection.privmsg(channel, 'To many matches, please refine your search')
+      else:
+        for line in matchbuf:
+          connection.privmsg(channel, line)
 
 # React to channel messages
   def on_pubmsg ( self, connection, event ):
-    print event.arguments()
-    print event.target()
-    print event.eventtype()
-    print event.source()
 
 # Check for a !quotes command
     if self.listquotes.match(event.arguments()[0])!=None:
      result=event.arguments()[0].split(' ')
      connection.privmsg(channel, 'Im quotes')
     elif self.quote.match(event.arguments()[0]):
-       self.reportMatches(event.arguments()[0].split(' ')[1])
+       self.reportMatches(event.arguments()[0].split(' ')[1], connection)
+
 #check for a quit command 
     elif self.quitreg.match(event.arguments()[0]):
       self.die('Remote User Kill');
+
 #check for a this (Super secret function here)
     elif self.spartareg.match(event.arguments()[0]) and self.spartamode:
       connection.privmsg(channel, 'IS')
       connection.privmsg(channel, 'SPARTA')
       connection.privmsg(channel, '\001ACTION kicks '+event.source().split('!')[ 0 ]+' into a pit\001')
+
+#otherwise put the line into the linebuf and increment counter
     else:
-      self.linebuf.insert(self.counter, event.arguments()[0])
+      self.linebuf.insert(self.counter, event)
       self.counter=self.counter+1
       if self.counter>=self.bufSize:
         self.counter=0
