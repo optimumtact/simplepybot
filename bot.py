a7299ff6e48ddf6d261d7c38642ecef8a91517fe
Francis Devine <optimumtact@gmail.com>
2012-01-19 23:04:06 +1300

    more work on python 3 bot

--- bot.py
+++ bot.py
@@ -1,144 +1,57 @@
-#!/usr/bin/env python2
-import ircbot
-import re
-import shelve
-# Connection information
-network = 'segfault.net.nz'
-port = 6667
-channel = '#bots'
-nick = 'quotebot'
-name = 'BotHerd'
+#!/usr/bin/env python
+import socket
+import select
+import io
+network='segfault.net.nz'
+port=6667
+channel=['#bots']
+nick='quotebotv2'
+name='python3isformal'
+bufsize=4096
+incomplete_buffer=''
+address=(network,port)
 
-# Create our bot class
-   
-spartamode=False #enable/disable spartamode?
+
+def recv(socket):
+  d = socket.recv(bufsize)
+  data=d.decode('utf-8', 'replace')
+  if data:
+    process_data(data)
+
+def process_data(data):
+  global incomplete_buffer
+  if incomplete_buffer:
+    data=incomplete_buffer+data
+  if data[-2:]== '\r\n':
+    split_data=data.split('\r\n')
+  else:
+    split_data=data.split('\r\n')
+    incomplete_buffer=split_data.pop(-1)
+  for line in split_data:
+    if line:
+      if line.startswith('PING'):
+        parameters= line.split()
+        pong='PONG '+parameters[1]
+        send(pong)
+        return
+      handle_message(line)
+
+def send(line, socket, encode="utf-8"):
   
-linebuf=[] #stores the last bufSize messages from the channel
-    
-#check for !quotes followed by an irc nick and an optional id
-listquotes=re.compile('^!quotes [A-Za-z_]+ *[0-9]*$')
-    
-quote=re.compile('^!quote [\w+\s?]+$') #check for a !quote followed by a number of words and spaces 
+  line = line.replace('\r', '')
+  line = line.replace('\n', '')
+  line = line.replace('\r\n', '') + '\r\n'
+  socket.send(line.encode(encode))
 
-quitreg=re.compile(nick+':? quit') #check for my nickname followed by a optional colon and then quit
-    
-spartareg=re.compile('this') #check for a single this
-    
-counter=0 #current number of messages remembered
-    
-bufSize=100 #sets the maximum size of the bots channel memory (number of messages remembered)
-   
-#Quote id num
-quoteID=1
-#open dict storing id's with a single quote attached to each id from file, creating this file if it does not
-#exist
-quoteDict=shelve.open('ircquotes', flag='c') 
-#open dict storing names with a list of ids associated to each name (where each id is a entry in the quoteDict
-#corresponding to a quote from that name) from file, creating the file if it does not exist
-nameDict=shelve.open('nameDict', flag='c') 
-    
-  
-  # Bot Methods below
-  # Join the channel when connected
-  def on_welcome (  connection, event ):
-    connection.join ( channel )
-  
-
-  # React to channel messages
-  def on_pubmsg ( self, connection, event ):
-    #store the actual message from the event
-    message=getMsg(event)
-    
-    # Check for a !quotes command
-    if listquotes.match(message)!=None:
-     result=message.strip(' ').split(' ')
-     connection.privmsg(channel, lookupQuote(result))
-    
-    #check for a !quote user command
-    elif quote.match(message):
-      match=findMatches(message.split(' ')[1], connection)
-      if match == None:
-        connection.privmsg(channel, 'test')
-      else:
-        connection.privmsg(channel, addQuote(self.quoteID, message.split(' ', 1)[1],  event.source().split('!')[0]))
-        quoteID=self.quoteID+1
-    #check for a quit command 
-    elif quitreg.match(message):
-      die('Remote User Kill');
-
-    #check for a this (Super secret function here)
-    elif spartareg.match(message) and self.spartamode:
-      connection.privmsg(channel, 'IS')
-      connection.privmsg(channel, 'SPARTA')
-      connection.privmsg(channel, '\001ACTION kicks '+event.source().split('!')[ 0 ]+' into a pit\001')
-
-#otherwise put the line into the linebuf and increment counter
-    else:
-      print event.arguments()
-      print event.source()
-      print event.target()
-      print event.eventtype()
-      linebuf.insert(self.counter, event)
-      counter=self.counter+1
-      if counter>=self.bufSize:
-        counter=0
-#handle private msg commands
-  def on_privmsg(self, connection, event):
-    if(spartamode==True):
-      spartamode=False
-    else:
-      spartamode=True
-
-#Utility Methods
-  
-  #get the message out of the event object
-  def getMsg(self, event):
-    return event.arguments()[0]
-
-  #turn event objects into nicely formatted strings
-  def formatForDisplay(self, event):
-    displaystring='<'+event.source().split('!')[0]+'> '+event.arguments()[0]
-    return displaystring
-
-  #handle reporting of matching quotes
-  def findMatches(self, pattern, connection):
-      matchbuf=[]
-      for event in linebuf:
-        if re.search(pattern, event.arguments()[0])!=None:
-          matchbuf.append(formatForDisplay(event))
-      if (len(matchbuf)>1):
-        connection.privmsg(channel, 'Too many matches, please refine your search')
-      else:
-        return matchbuf
-
-  #handle insertion of new quote, as well as placing it into the appropriate name and date dicts
-  def addQuote(self, idnum, quote, name):
-    quoteDict[idnum]=quote
-    if name in nameDict:
-      temp=nameDict[name]
-      temp.append(idnum)
-      nameDict[name]=temp
-    else:
-      nameDict[name]=[idnum]
-    
-    print quote
-    print name
-    print idnum
-    return 'q'
-
-  def lookupQuote(self, reference):
-    if len(reference)==3:
-      if reference[2] in quoteDict:
-        return quoteDict[reference[2]]
-      else:
-        return 'I do not have a quote with id:'+reference[2]
-    else:
-      if reference[1] not in nameDict :
-        return 'I do not have a user with the name:'+reference[1]+' in the database'
-      ids=nameDict[reference[1]]
-      for quoteid in ids:
-        quotes+' '+quoteDict[quoteid]
-      return quotes
-
-bot = QuoteBot ([(network, port)], nick, name )
-bot.start()
+def handle_message(message):
+  print(message)
+def connect(message):
+  global address
+  server=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+  server.connect(address)
+  send('NICK '+nick, server)
+  send('USER francis devines segfault :Francis bot', server)
+  return server
+server=connect('true')
+while True:
+  recv(server)
