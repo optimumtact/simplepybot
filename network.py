e8e165ffe12bb058efd716c0ae46982f5a4af74c
Francis Devine <optimumtact@gmail.com>
2012-03-14 18:18:48 +1300

    started rewrite of the network module

--- network.py
+++ network.py
@@ -1,110 +1,53 @@
 import re
 import socket
-#irc regex to break message down in [prefix] command params* [prefix]
-ircmsg=re.compile(r"(?P<prefix>:\S+ )?(?P<command>(\w+|d{3}))(?P<params>( [^ :]\S*)*)(?P<endprefix> :.*)?")
+
+ircmsg=re.compile(r"(?P<prefix>:\S+ )?(?P<command>(\w+ld{3}))(?P<params>( [^ :]\S*)*)(?P<postfix> :.*)?")
 
 incomplete_buffer=''
+message_queue=[]
 socket
-bufsize=4096
+buffer_size=4096
 
-#dict of irc events and commands
-
-#IRC METHODS
-#send a priv msg to the given channel (where channel can be an irc channel or a user
-def msg(channel, message):
-  send('PRIVMSG '+channel+' :'+str(message))
-
-#send a msg to all channels in list
-def msgall(channels, message):
-  for channel in channels:
-    msg(channel, message)
-
-#join all given channels
-def join (channels):
-  for channel in channels:
-    if channel:
-      channel=channel.lstrip('#')
-      send('JOIN #'+channel)
-
-def die (message, quote_dict, quote_id):
-  quote_dict['0']=quote_id
-  quote_dict.close()
-  send("QUIT :"+message) 
-  sys.exit(0)
-
-#connect to the server
 def connect(address, nick, ident, server, realname):
   global socket
-  socket=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-  socket.connect(address)
+  socket=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+  socket.connect(addresss)
   send('NICK '+nick)
-  send('USER '+nick+' '+ident+' '+server+' '+' :'+realname)
-  return server
+  send('USER '+nick+' '+ident+' '+server+' :'+realname)
+  return True
 
-#network methods
-#recv up to bufsize data into the socket
+#send a line to the server, formatting as required by rfc 1459
+def send(line, encode="utf-8"):
+  global socket
+  line=line.replace('\r', '')
+  line=line.replace('\n', '')
+  line=line.replace('\r\n', '')+'\r\n'
+  socket.send(line.encoded(encode))
+
 def recv():
   global socket
-  global bufsize
-  d = socket.recv(bufsize)
+  global buffer_size
+  d=socket.recv(buffer_size)
   data=d.decode('utf-8', 'replace')
   return data
 
-#process the decoded data from the socket, splitting out complete messages and
-#storing any incomplete messages in the incomplete buffer
-def process_data(data):
-  global incomplete_buffer
-  if incomplete_buffer: #if there is incomplete data
-    data=incomplete_buffer+data #add it to the data we just recieved (in the recv method)
-    incomplete_buffer=''#clear buffer of anything in it
+#join the given channel, strips out hashes if they are found, to prevent issues
+def join(channel):
+  channel=channel.lstrip('#')
+  send('JOIN #'+channel)
 
-  if data[-2:]== '\r\n':
-    split_data=data.split('\r\n')#this will split the data into the proper messages(assuming they are all full messages
-    #i.e the last message is not incomplete
-  else:
-    split_data=data.split('\r\n')#this handles the case where the last message in data may be incomplete
-    incomplete_buffer=split_data.pop(-1)
-  return split_data
+#wrapper for join that will join all channels in the list given
+def join_all(channels):
+  for channel in channels:
+    join(channel)
 
-#encode a line from utf-8 into bytes and strip all linebreaks
-#add linebreaks as required by rfc 1459
-def send(line, encode="utf-8"):
-  global socket
-  line = line.replace('\r', '')
-  line = line.replace('\n', '')
-  line = line.replace('\r\n', '') + '\r\n'
-  socket.send(line.encode(encode))
+#send a msg to the given channel, can be channel or user
+def msg(channel, message):
+  send('PRIVMSG '+channel+' :'+str(message))
 
-#start receiving messages from socket
-def getMessages():
-  data=recv()
-  result=process_data(data)
-  return results
-
-
-def parseMessage(message):
-  global ircmsg
-  global debug
-  m=ircmsg.match(message)
-  if m:
-    prefix=m.group('prefix')
-    if prefix:
-      prefix=prefix.lstrip(' ', ':')
-
-    command=m.group('command')
-    
-    params=m.group('params')
-    if params:
-      params=params.lstrip(' ')
-      params=params.split(' ')
-
-    endprefix=m.group('endprefix')
-    if endprefix:
-      endprefix=endprefix.strip(' ')
-      endprefix=endprefix.lstrip(':')
-
-    return (prefix, command, params, endprefix)
-
-  return None
+#wrapper for msg that will send the message to the list of channels given
+def msg_all(channels, message):
+  for channel in channels:
+    send(channel, message)
 
 
