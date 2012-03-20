6611d272505fd956a481b5a9144f31362ba54e02
Francis Devine <optimumtact@gmail.com>
2012-03-20 18:50:09 +1300

    made linebuffer respect the buffer_size variable

--- linebuffer.py
+++ linebuffer.py
@@ -1,19 +1,27 @@
+import re
 channel_dictionary=dict()
-max_supported_channels=None
-buffer_size=None
+max_channels=None
+buffer_size=20
 
 
-def intialise(max_channels=-1, max_buffer_size=-1):
-  global max_supported_channels
-  max_supported_channels=max_channels
-  global buffer_size
-  buffer_size=max_buffer_size
+def intialise(max_channel_size=-1, max_buffer_size=-1):
+  global max_channels
+  max_channels=max_channel_size
+  if max_buffer_size>0:
+    global buffer_size
+    buffer_size=max_buffer_size
 
 
 def add_channel(channel_name):
+  global max_channels
+  if len(channel_dictionary)>=max_channels:
+    return False
+
   global channel_dictionary
+  global buffer_size
   if channel not in channel_dictionary:
-    channel_dictionary[channel_name]=[]
+    #intialise the channel with it's message count value
+    channel_dictionary[channel_name]=[0]
     return True
 
   else:
@@ -33,8 +41,25 @@ def add_line(channel_name, line):
   global channel_dictionary
   #this will add the channel if it does not yet exist
   add_channel(channel_name)
+
+  #get the channel we want to add the line too
+  channel=channel_dictionary[channel_name]
+
   #now we append the line to the channel
-  channel_dictionary[channel_name].append(line)
+  add_line_to_channel(channel, line)
+
+
+def add_line_to_channel(channel, line):
+  global buffer_size
+  count=channel[0]
+  
+  if count>=buffer_size:
+    count=0
+
+  channel[count+1]=line
+  count=count+1
+  channel[0]=count
+    
 
 
 def find_lines(channel_name, regex):
