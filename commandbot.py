3b95b2a8d661f359c9ce64cdfe67630f1a5afed4
Francis <Francis.Devine@ecs.vuw.ac.nz>
2012-09-07 22:47:13 +1200

    rolled pointless irc file into commandbot

--- commandbot.py
+++ commandbot.py
@@ -1,4 +1,4 @@
-import irc
+from network import IrcSocket
 import re
 from time import sleep
 
@@ -17,7 +17,7 @@ def command(expr, func):
         return True
     return process
 
-class CommandBot(irc.IrcConnection):
+class CommandBot(IrcSocket):
     '''
     A base IRC bot with a simple framework and helpers in place for command processing.
 
@@ -27,7 +27,12 @@ class CommandBot(irc.IrcConnection):
     '''
     commands = []
     def __init__(self, nick, network, port):
-        super(CommandBot, self).__init__(nick, network, port)
+        super(CommandBot, self).__init__()
+        assert network and port
+        self.connect((network, port), nick, "bot@"+network, network, nick)
+        self.name = nick
+        self.server = network
+        self.port = port
 
     def loop(self):
         '''
@@ -41,6 +46,15 @@ class CommandBot(irc.IrcConnection):
             self.logic()
             sleep(.1)
 
+    """
+    Log messages inside an internal database for later retrieval	
+    """
+    def log_message(self, source, action, targets, message, m):
+        #not implemented yet
+        pass
+
+
+
     def logic(self):
         '''
         Simple logic processing.
@@ -51,6 +65,57 @@ class CommandBot(irc.IrcConnection):
             source, action, targets, message = m
             print(m)
             if message and action == "PRIVMSG":
+                self.log_message(source, action, targets, message, m)
                 for c in self.commands:
                     if c(source, action, targets, message):
                         break
+                    
+
+        return
+
+
+    def msgs_all(self, msgs, channels):
+        """
+        Accepts a list of msgs to send to a list of channels
+        """
+        for channel in channels:
+            for message in msgs:
+                self.msg(message, channel)
+
+    def msg_all(self, message, channels):
+        """
+        Accepts a message to send to a list of channels
+        """
+        for channel in channels:
+            self.msg(message, channel)
+
+    def msg(self, message, channel):
+        '''
+        Send a message to a specific target.
+        '''
+        self.send('PRIVMSG ' + channel + ' :' + message)
+
+    def join(self, channel):
+        '''
+        Join a channel.
+
+        The channel should contain one or more # symbols as needed.
+        '''
+        self.send('JOIN ' + channel)
+
+    def quit(self, message):
+        '''
+        Disconnects from a server with a given QUIT message.
+        '''
+        self.send('QUIT :' + message)
+
+    def leave(self, channel, message):
+        '''
+        Leaves a channel, optionally sending a message to the channel first.
+
+        XXX: The message should probably be the PART message
+        '''
+        if message:
+            self.msg(message, channel)
+        self.send('PART ' + channel)
+
