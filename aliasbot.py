6116a248b4d43cadb73fe8767e3b2f1134e65778
Francis <Francis.Devine@ecs.vuw.ac.nz>
2012-09-07 23:09:03 +1200

    removed DB class from aliasbot, moved it to commandbot, updated aliasabot to reflect this, plus other changes

--- aliasbot.py
+++ aliasbot.py
@@ -1,16 +1,6 @@
 from commandbot import *
-import dbm
 import sys
 
-class QuoteDB:
-    '''
-    Trivial wrapper class over dbm to enable use in with statements.
-    '''
-    def __enter__(self):
-        self._internal = dbm.open('alias', 'c')
-        return self._internal
-    def __exit__(self, type, value, traceback):
-        self._internal.close()
 
 class AliasBot(CommandBot):
     '''
@@ -24,13 +14,13 @@ class AliasBot(CommandBot):
     nick = "gamzee"
     def __init__(self, network, port):
         self.commands = [
-	    command(r"^%s: quit" %self.nick, self.end),
-        command(r"^%s:" % self.nick, self.honk),
-        command(r"^!learn (?P<abbr>\S+) as (?P<long>\S.*)$", self.learn),
-        command(r"^!forget (?P<abbr>\S+)", self.forget),
-	    command(r"^!list_abbr$", self.list_abbrievations),
-        command(r"^!(?P<abbr>\S+)$", self.retrieve)
-        ]
+                command(r"^%s: quit" %self.nick, self.end),
+                command(r"^%s:" % self.nick, self.honk),
+                command(r"^!learn (?P<abbr>\S+) as (?P<long>\S.*)$", self.learn),
+                command(r"^!forget (?P<abbr>\S+)", self.forget),
+                command(r"^!list_abbr$", self.list_abbrievations),
+                command(r"^!(?P<abbr>\S+)$", self.retrieve)
+                ]
         super(AliasBot, self).__init__(self.nick, network, port)
         self.honk = "HONK"
 
@@ -76,21 +66,21 @@ class AliasBot(CommandBot):
             self.msg_all("Sorry, I don't know about %s." % command, targets)
 
     def end(self, source, action, targets, message, m):
-	"""
-	Quits the server
-	"""
-	self.quit("I can code something!")
-	sys.exit(0)
-    
+        """
+        Quits the server
+        """
+        self.quit("I can code something!")
+        sys.exit(0)
+
     def list_abbrievations(self, source, action, targets, message, m):
         """
-	List all known abbrievations
-	"""
-	keys = ", ".join(self.quotes.keys())
-	self.msg_all(keys, targets)
-	 
+        List all known abbrievation commands
+        """
+        keys = ", ".join(self.quotes.keys())
+        self.msg_all(keys, targets)
+
     def loop(self):
-        with QuoteDB() as self.quotes:
+        with BotDB('alias') as self.quotes:
             super(AliasBot, self).loop()
 
 qb = AliasBot("irc.segfault.net.nz", 6667)
