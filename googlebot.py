7bc6f6095ed45b47e5189cef0a8214ed0b8fe45b
Francis <Francis.Devine@ecs.vuw.ac.nz>
2012-09-09 22:43:09 +1200

    added a very simple bot that will provide ltmgtfy links on demand, snarkiness at it's laziest

---
+++ googlebot.py
@@ -0,0 +1,34 @@
+from commandbot import *
+import sys
+
+class GoogleBot(CommandBot):
+    """
+    An IRC bot that can when asked, return a link that will search
+    google for the asked terms, greatly improves ability to sarcastically
+    answer peoples questions
+    """
+
+    nick = "SarcasticHelper"
+    def __init__(self, network, port):
+        self.commands = [
+                command(r"^%s: quit" % self.nick, self.end),
+                command(r"!help (?P<searchterms>\S+)", self.return_search_link)
+                ]
+        self.network = network
+        self.port = port
+        super(GoogleBot, self).__init__(self.nick, self.network, self.port)
+
+    def end(self, source, action, targets, message, m):
+        """
+        Quits the server!
+        """
+        self.quit("No more snarkiness")
+        sys.exit(0)
+
+    def return_search_link(self, source, action, targets, message, m):
+        if m.group("searchterms"):
+            self.msg_all("http://lmgtfy.com/?q="+m.group("searchterms"), targets)
+
+hb = GoogleBot("irc.segfault.net.nz", 6667)
+hb.join("#cave")
+hb.loop()
