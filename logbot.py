b161b4de71c24d2d4517b124b8ee409574093997
Francis <optimumtact@gmail.com>
2012-10-23 18:28:03 +1300

    turned logbot into a module

--- logbot.py
+++ logbot.py
@@ -8,58 +8,61 @@ class LogBot(CommandBot):
     """
 
     nick = "LumberJack"
-    def __init__(self, network, port):
+    def __init__(self, bot):
+        self.bot = bot
         self.commands = [
                 command(r"^%s: quit" % self.nick, self.end),
                 command(r"^!harvest many (?P<match>.*)", self.harvest_many),
                 command(r"^!harvest (?P<match>.*)", self.harvest)
                 ]
-        super(LogBot, self).__init__(self.nick, network, port)
+        
 
     def harvest_many(self, source, actions, targets, message, m):
-        """
+        '''
         Search the logs for every item that can .search match the m.group("match")
         value
-        """
+        '''
         messages = []
         try:
-            results = self.search_logs_greedy(m.group("match"), match=False)
-                if results:
-                    for result in results:
-                        messages.append (" [message:{0}, sender:{1}] ".format(result.message, result.name))
-                    self.msg_all(r"".join(messages), targets)
+            results = self.bot.search_logs_greedy(m.group("match"), match=False)
+            if results:
+                for result in results:
+                    messages.append (" [message:{0}, sender:{1}] ".format(result.message, result.name))
+                self.bot.msg_all(''.join(messages), targets)
 
-                else:
-                    self.msg_all("No matches found", targets)
+            else:
+                self.bot.msg_all("No matches found", targets)
 
         except re.error:
-            self.msg_all("Not a valid regex", targets)
+            self.bot.msg_all("Not a valid regex", targets)
 
     def harvest(self, source, actions, targets, message, m):
         """
         Search the logs for anything matching the m.group("match") value
         """
         try:
-            result = self.search_logs(m.group("match"), match=False)
-                if result:
-                    message = "Harvested:{0}, sender:{1}".format(result.message, result.name)
-                    self.msg_all(message, targets)
+            result = self.bot.search_logs(m.group("match"), match=False)
+            if result:
+                message = "Harvested:{0}, sender:{1}".format(result.message, result.name)
+                self.bot.msg_all(message, targets)
 
-                else:
-                    self.msg_all("No match found", targets)
+            else:
+                self.bot.msg_all("No match found", targets)
 
         except re.error:
-            self.msg_all("Not a valid regex", targets)
+            self.bot.msg_all("Not a valid regex", targets)
 
 
     def end(self, source, actions, targets, message, m):
         """
         Quit server and kill script
         """
-        self.quit("Lunch Break")
+        self.bot.quit("Lunch Break")
         sys.exit(0)
 
-hb = LogBot("irc.segfault.net.nz", 6667)
-hb.join("#bots")
-hb.loop()
+bot = CommandBot('Harvester', 'irc.segfault.net.nz', 6667)
+bot.join('#bots')
+hb = LogBot(bot)
+bot.add_module('Logging', hb)
+bot.loop()
 
