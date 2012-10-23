25f66dc0723c38e16385ed49b0b86fb1af87f900
Francis <optimumtact@gmail.com>
2012-10-23 19:00:38 +1300

    made googlebot a module

---
+++ googlemodule.py
@@ -0,0 +1,26 @@
+from commandbot import *
+import sys
+
+class GoogleModule():
+    """
+    An IRC module that can return a link that will search
+    google for the asked terms, greatly improves ability 
+    to sarcastically answer peoples questions
+    """
+
+    def __init__(self, bot):
+        self.bot = bot
+        self.commands = [
+                command(r"!help (?P<searchterms>\S+)", self.return_search_link)
+                ]
+
+    def return_search_link(self, source, action, targets, message, m):
+        search_terms = m.group("searchterms").replace(" ", "+")
+        self.bot.msg_all("http://lmgtfy.com/?q="+m.group("searchterms"), targets)
+
+
+bot = CommandBot("HelpBot", "irc.segfault.net.nz", 6667)
+bot.join("#cave")
+gb = new GoogleBot()
+bot.add_module("Helper", gb)
+bot.loop()
