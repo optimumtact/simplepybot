6b839af238538c8e9a11286c36c67be11f03e228
Francis <optimumtact@gmail.com>
2013-03-16 16:08:50 +1300

    brought quotebot up to new module standard and updated it's storage semantics

--- quotebot.py
+++ quotebot.py
@@ -1,9 +1,9 @@
 from commandbot import *
+from log_search import LogModule
 import shelve
 
 #TODO implement delete command for quotes, clean up quote command syntax
-#TODO add a quit command
-class QuoteBot(CommandBot):
+class QuoteBot():
     """
     A bot that provides quote services to a channel
 
@@ -15,17 +15,20 @@ class QuoteBot(CommandBot):
 
     """
     nick = "quotebot"
-    def __init__(self, network, port):
+    def __init__(self, bot, module_name='Quotes'):
         self.commands = [
                 command(r"!quote (?P<id>\d+) by (?P<nick>\S+)", self.quote_by_id),
                 command(r"^!quote (?P<match>[\w\s]+) by (?P<nick>\S+)", self.find_and_remember_quote_by_name),
                 command(r"!quotes for (?P<nick>\S+)", self.quote_ids_for_name),
                 command(r"^!quote (?P<match>[\w\s]+)", self.find_and_remember_quote),
                 ]
-        self.network = network
-        self.port = port
-        super(QuoteBot, self).__init__(self.nick, self.network, self.port)
-    
+        self.events = []
+
+        self.module_name = module_name
+        self.bot = bot
+        self.log_module = bot.get_module('Logging')
+        bot.add_module(module_name, self)
+
     def quote_by_id(self, source, action, targets, message, m):
         """
         Determine if the id given by m.group("id") is assigned for m.group("nick")
@@ -33,21 +36,23 @@ class QuoteBot(CommandBot):
         """
         nick = str(m.group("nick"))
         quote_id = int(m.group("id"))
-        if nick in self.quotedb:
-            quote_ids = self.quotedb[nick].get_all_ids()
+        ids_index = str((self.module_name, nick))
+
+        if ids_index in self.bot.storage:
+            quote_ids = self.bot.storage[ids_index]
             if quote_id in quote_ids:
-                index = str((nick, quote_id))
-                if index in self.quotedb:
-                    self.msg_all(str(self.quotedb[index]), targets)
+                index = str((self.module_name, (nick, quote_id)))
+                if index in self.bot.storage:
+                    self.bot.msg_all(str(self.bot.storage[index]), targets)
 
                 else:
-                    self.msg_all('error', targets)
+                    self.bot.msg_all('Missing quote for index:'+index, targets)
 
             else:
-                self.msg_all("There is no quote with that id for this user", targets)
+                self.bot.msg_all("There is no quote with that id for this user", targets)
 
         else:
-            self.msg_all("There are no quotes for that user", targets)
+            self.bot.msg_all("There are no quotes for that user", targets)
 
     def quote_ids_for_name(self, source, action, targets, message, m):
         """
@@ -55,35 +60,36 @@ class QuoteBot(CommandBot):
         and is so return a list of them to the channel for display
         """
         nick = str(m.group("nick"))
-        if nick in self.quotedb:
-            ids = self.quotedb[nick].get_all_ids()
+        index = str((self.module_name, nick))
+        if index in self.bot.storage:
+            ids = self.bot.storage[index]
             strids = list()
             for x in ids:
                 strids.append(str(x))
-            self.msg_all('['+','.join(strids)+']', targets)
+            self.bot.msg_all('['+','.join(strids)+']', targets)
 
         else:
-            self.msg_all("No ID's for nickname:"+nick)
+            self.bot.msg_all("No ID's for nickname:"+nick)
 
     def find_and_remember_quote_by_name(self, source, action, targets, message, m):
         '''
         Search the channel and find the quote to store
         '''
         try:
-            results = self.search_logs_greedy(m.group('match'), name = m.group('nick'))
+            results = self.log_module.search_logs_greedy(m.group('match'), name = m.group('nick'))
             if results:
                 if len(results) > 1:
-                    self.msg_all('Too many matches found, please refine your search', targets)
+                    self.bot.msg_all('Too many matches found, please refine your search', targets)
 
                 else:
                     entry = self.store_quote(source, results[0])
-                    self.msg_all('Stored:'+str(entry), targets)
+                    self.bot.msg_all('Stored:'+str(entry), targets)
 
             else:
-                self.msg_all('No match found, please try another query', targets)
+                self.bot.msg_all('No match found, please try another query', targets)
 
         except re.error:
-            self.msg_all('Not a valid regex, please try another query', targets)
+            self.bot.msg_all('Not a valid regex, please try another query', targets)
 
 
     def find_and_remember_quote(self, source, action, targets, message, m):
@@ -95,22 +101,22 @@ class QuoteBot(CommandBot):
         regex is given by m.group("match")
         """
         try:
-            results = self.search_logs_greedy(m.group("match"))
+            results = self.log_module.search_logs_greedy(m.group("match"))
             if results:
                 if len(results) > 1:
-                    self.msg_all("Too many matches found, please refine your search", targets)
+                    self.bot.msg_all("Too many matches found, please refine your search", targets)
 
                 else:
                     #if only one match store the quote along with some supporting info
                     #reminder: results[0] is a log entry instance, see commandbot for source of this
                     entry = self.store_quote(source, results[0])
-                    self.msg_all("Stored:"+str(entry), targets)
+                    self.bot.msg_all("Stored:"+str(entry), targets)
 
             else:
-                self.msg_all('No match found, please try another query', targets)
+                self.bot.msg_all('No match found, please try another query', targets)
 
         except re.error:
-            self.msg_all('Not a valid regex, please try another query', targets)
+            self.bot.msg_all('Not a valid regex, please try another query', targets)
 
     def store_quote(self, source, entry):
         """
@@ -125,112 +131,54 @@ class QuoteBot(CommandBot):
         #check to see if this user exists and get the id if he does
         #otherwise we store this new user and the current id
         quote_id = 0
-        #convert to string as shelve+dbm doesn't support unicode
+        #convert to string for indexing
         name = str(entry.name)
-        if name in self.quotedb:
-            quote_data = self.quotedb[name]
-            quote_id = quote_data.get_next_id()
-            self.quotedb[name] = quote_data
-        
+        index = str((self.module_name, name))
+        if index in self.bot.storage:
+            #get the list of used ids
+            quote_data = self.bot.storage[index]
+            #search through it to find first gap or next value
+            quote_id = find_next_id(quote_data)
+            #insert the quote_id into the list and store the newly changed list
+            #we use insert ast the find_next_id method expects a sorted list
+            quote_data.insert(quote_id, quote_id)
+            self.bot.storage[index] = quote_data
+
         else:
-            #initialise this new users necessary data
-            self.quotedb[name] = QuoteData(name)
-        
+            #initialise this new users ids list
+            self.bot.storage[index] = [0]
+
         #convert the combo of entry name + quote_id into a string to use as the index
-        index = str((name, quote_id))
-        #If this user and id combination already exists we have a serious problem
-        if index in self.quotedb:
+        index = str((self.module_name,(name, quote_id)))
+        #If this user and id combination already exists we have a collision
+        if index in self.bot.storage:
             #TODO raise a nicer exception here
-            raise Exception("User and ID combination already exists")
-        
+            raise Exception("User and ID collision:"+str((name, quote_id)))
+
         else:
-            self.quotedb[index] = entry
+            self.bot.storage[index] = entry
             return entry
 
-    def loop(self):
-        with BotShelf('stored_quotes') as self.quotedb:
-            super(QuoteBot, self).loop()
-
-
-
-class QuoteData:
-    """
-    This class handles the id management for each nick in the database
-    it provide support methods for freeing an id, getting the next id
-    to use and providing a list of all id's.
-    """
-
-    def __init__(self, nick):
-        self.current_id = 0
-        self.nick = nick
-        self.freed = list()
-        self.in_use = list()
-        self.in_use.append(self.current_id)
-
-    def get_next_id(self):
-        """
-        Return the next id for use as part of the (nick, id) tuple
-        As well as rememebering that it is in use
-        """
-        quote_id = 0
-
-        #if there are any freed id's floating around we should use them
-        #over advancing the current_id counter, this helps decrease the
-        #chances of eventually running out of id's
-        if len(self.freed):
-            quote_id = self.freed.pop()
-            self.in_use.append(quote_id)
-            return quote_id
-        
-        #we use the current id + 1 and increment the current id counter
+def find_next_id(id_list):
+    length = len(id_list)
+    for index, q_id in enumerate(id_list):
+        if index+1 < length:
+            #while we still can move along
+            next_id = id_list[index+1]
         else:
-            quote_id = self.current_id + 1
-            self.current_id = quote_id
-            self.in_use.append(quote_id)
-            return quote_id
+            #end of list, return q_id + 1
+            next_id = q_id + 1
+            return q_id + 1
 
-    def free_id(self, quote_id):
-        if quote_id not in self.in_use:
-            return False
+        if q_id + 1 < next_id:
+            # we found a gap in id's
+            return q_id + 1
 
-        else:
-            self.in_use.remove(quote_id)
-            self.freed.append(quote_id)
-            return True
-
-    def get_all_ids(self):
-        return self.in_use
-
-    def __str__(self):
-        return self.nick+":"+str(self.current_id)
-
-class BotShelf:
-    '''
-    Wrapper class over shelve to allow it to be used
-    in with statements
-    '''
-
-    def __init__(self, name):
-        '''
-        Set up new BotShelf
-        '''
-        self.name = name
-
-    def __enter__(self):
-        '''
-        Open shelve instance with self.name as filename
-        creating it if it doesn't already exist
-        '''
-        self._internal = shelve.open(self.name, 'c')
-        return self._internal
-
-    def __exit__(self, type, value, traceback):
-        '''
-        On exit close internal db instance
-        '''
-        self._internal.close()
-
-# run bot
-qb = QuoteBot("irc.segfault.net.nz", 6667)
-qb.join("#bots")
-qb.loop()
+if __name__ == '__main__':
+    bot = CommandBot('betterthanscorecard', 'irc.segfault.net.nz', 6667)
+    #add the log and quote modules
+    #note Quotebot relies on log module
+    mod = LogModule(bot)
+    mod = QuoteBot(bot)
+    bot.join('#bots')
+    bot.loop()
