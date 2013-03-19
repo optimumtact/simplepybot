4888d9b021691bc8d272dd710692d49c1bf88e66
devinefran <Francis.Devine@ecs.vuw.ac.nz>
2013-03-19 13:55:13 +1300

    moved quote module to quote.py and unhardcoded db name for exploredb.py

--- exploredb.py
+++ exploredb.py
@@ -1,5 +1,3 @@
-#!/usr/bin/python2 -i
-
 import shelve
 import sys
 import os
@@ -7,14 +5,9 @@ if len(sys.argv) != 2:
     print("Usage is ./exploredb dbfile")
     sys.exit()
 
-print(os.getcwd())
-print(sys.argv)
-print(sys.argv[1])
-db = shelve.open('BOTDB')
-
+db = shelve.open(sys.argv[1])
 print("database intialiased as db")
-
-
+print("Use print_db(db) for a printout")
 def print_db(db):
     for key in db.keys():
         print(key, db[key])
