ea00a67b68a15362f6104318ca41bcd27a4a0584
Francis <francis@xeon.pw>
2013-06-21 11:03:49 +1200

    updated channel_control to new standard

--- channel_control.py
+++ channel_control.py
@@ -1,13 +1,14 @@
-from commandbot import event, command, CommandBot
+from commandbot import CommandBot
 
 class ControlModule():
 
-    def __init__(self, bot):
+    def __init__(self, bot, module_name="Chan_Control"):
         self.bot = bot
+        bot.add_module(module_name, self)
         self.commands = [
-                command(bot.nick+':? join channel (?P<channel>[#|\w]+)', self.join),
-                command(bot.nick+':? leave channel (?P<channel>[#|\w]+)', self.leave),
-                command(bot.nick+':? msg user (?P<user>\w+) (?P<message>[ |\w]+', self.msg_user),
+                bot.command('join channel (?P<channel>[#|\w]+)', self.join, direct=True),
+                bot.command('leave channel (?P<channel>[#|\w]+)', self.leave, direct=True),
+                bot.command('annoy user (?P<user>\w+) (?P<message>.+)', self.msg_user, direct=True),
                 ]
         self.events = []
         self.channels = []
@@ -25,6 +26,9 @@ class ControlModule():
         if channel not in self.channels:
             self.bot.join(channel)
             self.channels.append(channel)
+            
+        else:
+            self.bot.msg_all("I am in that channel already", targets)
 
     def leave(self, source, action, targets, message, m):
         '''
@@ -37,15 +41,22 @@ class ControlModule():
         if channel in self.channels:
             self.bot.leave(channel)
             self.channels.remove(channel)
+            self.bot.msg_all("Left that channel", targets)
+        
+        else:
+            self.bot.msg_all("I am not in that channel", targets)
 
     def msg_user(self, source, action, targets, message, m):
         '''
         Send the user a message from the bot
         '''
         self.bot.msg(m.group('message'), m.group('user'))
+    
+    def quit():
+        pass #Nothing special
 
 if __name__ == '__main__':
-    bot = CommandBot('Annoy', 'irc.segfault.net.nz', 6667)
+    bot = CommandBot('Testing', 'irc.segfault.net.nz', 6667)
     bot.join('#bots')
-    mod = ControlModule('Command', bot)
+    mod = ControlModule(bot)
     bot.loop()
