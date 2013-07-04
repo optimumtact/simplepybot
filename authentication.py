2324f37cb1d2688922af361464cb3ecf34c61076
devinefran <Francis.Devine@ecs.vuw.ac.nz>
2013-07-04 14:42:28 +1200

    added simple auth provider for testing

---
+++ authentication.py
@@ -0,0 +1,10 @@
+class IdentAuth
+    def __init__(self):
+        pass
+
+    def is_allowed(nick, nickhost, level):
+        if nick == 'oranges':
+            return True
+
+        else:
+            return False
